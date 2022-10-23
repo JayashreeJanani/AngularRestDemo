8c4ab0de37ed29251745135b860803ab757056ab
JayashreeJanani <73242228+JayashreeJanani@users.noreply.github.com>
2022-10-23 15:08:19 +0530

    23-10-2022_15_07_work

--- src/app/add-resto/add-resto.component.ts
+++ src/app/add-resto/add-resto.component.ts
@@ -8,7 +8,7 @@ import { FormControl, FormGroup } from '@angular/forms'
   styleUrls: ['./add-resto.component.css']
 })
 export class AddRestoComponent implements OnInit {
-
+  alert : boolean= false;
   addRestorant= new FormGroup({
     name:new FormControl(''),
     address:new FormControl(''),
@@ -23,8 +23,12 @@ export class AddRestoComponent implements OnInit {
   addresto(){
     console.log(this.addRestorant.value);
     this.resta.addhotel(this.addRestorant.value).subscribe((result)=>{
+      this.alert=true;
+      this.addRestorant.reset({});
       console.log("Added",this.resta);
     });
+  } 
+  closeAlert(){
+    this.alert=false;
   }
-
 }
